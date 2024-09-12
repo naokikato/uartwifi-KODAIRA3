71c21d624026c707852b3a557790c51c25dbd216
Naoki Kato <naoki@bmoon.jp>
2024-09-12 13:38:46 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ uartwifi-KODAIRA3.ts
@@ -0,0 +1,123 @@
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#0fbc11 icon="" block="IFTTT連携"
+namespace IMLuartwifiv2 {
+    let isWifiConnected = false
+
+    //% block
+    //% weight=100
+    //% block="IFTTTに送信する|Event %event|Key %key|value1 %value1|value2 %value2|value3 %value3"
+    //% value1.defl="hello"
+    //% value2.defl="micro"
+    //% value3.defl="bit"
+    export function sendToIFTTT(value1: string, value2: string, value3: string) {
+        let result = 0
+        let retry = 2
+        let event = "spread_notify"
+        let key = "bdPxgUp39hNCdI4doqu4Lh"
+
+        // close the previous TCP connection
+        if (isWifiConnected) {
+            sendAtCmd("AT+CIPCLOSE")
+            waitAtResponse("OK", "ERROR", "None", 2000)
+        }
+
+        while (isWifiConnected && retry > 0) {
+            retry = retry - 1;
+            // establish TCP connection
+            sendAtCmd("AT+CIPSTART=\"TCP\",\"maker.ifttt.com\",80")
+            result = waitAtResponse("OK", "ALREADY CONNECTED", "ERROR", 2000)
+            if (result == 3) continue
+
+            let data = "GET /trigger/" + event + "/with/key/" + key
+            data = data + "?value1=" + value1
+            data = data + "&value2=" + value2
+            data = data + "&value3=" + value3
+            data = data + " HTTP/1.1"
+            data = data + "\u000D\u000A"
+            data = data + "User-Agent: curl/7.58.0"
+            data = data + "\u000D\u000A"
+            data = data + "Host: maker.ifttt.com"
+            data = data + "\u000D\u000A"
+            data = data + "Accept: */*"
+            data = data + "\u000D\u000A"
+
+            sendAtCmd("AT+CIPSEND=" + (data.length + 2))
+            result = waitAtResponse(">", "OK", "ERROR", 2000)
+            if (result == 3) continue
+            sendAtCmd(data)
+            result = waitAtResponse("SEND OK", "SEND FAIL", "ERROR", 5000)
+
+            // // close the TCP connection
+            // sendAtCmd("AT+CIPCLOSE")
+            // waitAtResponse("OK", "ERROR", "None", 2000)
+
+            if (result == 1) break
+        }
+    }
+
+    //% block
+    //% block="WiFiを設定する|TX %txPin|RX %rxPin|Baud rate %baudrate|SSID = %ssid|Password = %passwd"
+    //% weight=100    
+    //% txPin.defl=SerialPin.P15
+    //% rxPin.defl=SerialPin.P1
+    //% baudRate.defl=BaudRate.BaudRate115200    export function setupWifi(txPin: SerialPin, rxPin: SerialPin, baudRate: BaudRate, ssid: string, passwd: string) {
+    export function setupWifi() {
+        let result = 0
+        let txPin = SerialPin.P14
+        let rxPin = SerialPin.P8
+        let baudRate = BaudRate.BaudRate115200
+        let ssid = "BMOON-1F"
+        let passwd = "2002112100"
+
+        isWifiConnected = false
+
+        serial.redirect(
+            txPin,
+            rxPin,
+            baudRate
+        )
+
+        sendAtCmd("AT")
+        result = waitAtResponse("OK", "ERROR", "None", 1000)
+
+        sendAtCmd("AT+CWMODE=1")
+        result = waitAtResponse("OK", "ERROR", "None", 1000)
+
+        sendAtCmd(`AT+CWJAP="${ssid}","${passwd}"`)
+        result = waitAtResponse("WIFI GOT IP", "ERROR", "None", 20000)
+
+        if (result == 1) {
+            isWifiConnected = true
+        }
+    }
+
+    //% block
+    //% weight=100    
+    //% block="WiFiOK"
+    export function wifiOK(): boolean {
+        return isWifiConnected
+    }
+
+    function sendAtCmd(cmd: string) {
+        serial.writeString(cmd + "\u000D\u000A")
+    }
+
+    function waitAtResponse(target1: string, target2: string, target3: string, timeout: number) {
+        let buffer = ""
+        let start = input.runningTime()
+
+        while ((input.runningTime() - start) < timeout) {
+            buffer += serial.readString()
+
+            if (buffer.includes(target1)) return 1
+            if (buffer.includes(target2)) return 2
+            if (buffer.includes(target3)) return 3
+
+            basic.pause(100)
+        }
+
+        return 0
+    }
+}
